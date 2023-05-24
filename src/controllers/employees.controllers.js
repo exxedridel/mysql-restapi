889ccc42b9c9f953a571078e5fe1aeac64ef23c7
exxedridel <exxed.ridel@gmail.com>
2023-05-23 22:02:20 -0600

    ADD request errors handling functionality

--- src/controllers/employees.controllers.js
+++ src/controllers/employees.controllers.js
@@ -1,81 +1,112 @@
 import { pool } from "../db.js";
+// import 'express-promise-router' for handle errors better
 
 /* Read employees functionality */
 export const getEmployees = async (req, res) => {
-  const [rows] = await pool.query("SELECT * FROM employees");
-  res.send(rows);
+  try {
+    const [rows] = await pool.query("SELECT * FROM employees");
+    res.send(rows);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Something goes wrong",
+    });
+  }
 };
 
 /* Read 1 employee functionality */
 export const get1Employee = async (req, res) => {
-  const [rows] = await pool.query("SELECT * FROM employees WHERE id = ?", [
-    req.params.id,
-  ]);
+  try {
+    const [rows] = await pool.query("SELECT * FROM employees WHERE id = ?", [
+      req.params.id,
+    ]);
 
-  if (rows.length <= 0)
-    return res.status(404).json({
-      message: "Employee not found",
+    if (rows.length <= 0)
+      return res.status(404).json({
+        message: "Employee not found",
+      });
+
+    res.json(rows[0]);
+    /* SSR */
+    // res.send(
+    //   `<h1 style="
+    //       color: blue; text-align: center; padding-block: 45vh;
+    //     ">
+    //       Obtaining employee id: ${rows[0].id}
+    //   </h1>`
+    // );
+  } catch (error) {
+    return res.status(500).json({
+      message: "Something goes wrong",
     });
-
-  res.json(rows[0]);
-  /* SSR */
-  // res.send(
-  //   `<h1 style="
-  //       color: blue; text-align: center; padding-block: 45vh;
-  //     ">
-  //       Obtaining employee id: ${rows[0].id}
-  //   </h1>`
-  // );
+  }
 };
 
 /* Create employee functionality */
 export const createEmployee = async (req, res) => {
   const { name, salary } = req.body;
-  const [rows] = await pool.query(
-    "INSERT INTO employees(name, salary) VALUES (?, ?)",
-    [name, salary]
-  );
+  try {
+    const [rows] = await pool.query(
+      "INSERT INTO employees(name, salary) VALUES (?, ?)",
+      [name, salary]
+    );
 
-  /* This approach only returns the entered data not the actual row created in database */
-  res.send({
-    id: rows.insertId,
-    name,
-    salary,
-  });
+    /* This approach only returns the entered data not the actual row created in database */
+    res.send({
+      id: rows.insertId,
+      name,
+      salary,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Something goes wrong",
+    });
+  }
 };
 
 /* Patch employee functionality (to partially update the data) */
 export const updateEmployee = async (req, res) => {
   const { id } = req.params;
   const { name, salary } = req.body;
-  const [result] = await pool.query(
-    "UPDATE employees SET name = IFNULL(?, name), salary = IFNULL(?, salary)  WHERE id = ?",
-    [name, salary, id]
-  );
+  try {
+    const [result] = await pool.query(
+      "UPDATE employees SET name = IFNULL(?, name), salary = IFNULL(?, salary)  WHERE id = ?",
+      [name, salary, id]
+    );
 
-  if (result.affectedRows <= 0)
-    return res.status(404).send({ message: "Employee not found" });
+    if (result.affectedRows <= 0)
+      return res.status(404).send({ message: "Employee not found" });
 
-  /* This approach returns the actual row edited in the database */
-  const [rows] = await pool.query("SELECT * FROM employees WHERE id = ?", [id]);
-  res.json(rows[0]);
+    /* This approach returns the actual row edited in the database */
+    const [rows] = await pool.query("SELECT * FROM employees WHERE id = ?", [
+      id,
+    ]);
+    res.json(rows[0]);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Something goes wrong",
+    });
+  }
 };
 
 /* Delete employee functionality */
 export const deleteEmployee = async (req, res) => {
-  console.log(req.params.id);
+  try {
+    const [result] = await pool.query("DELETE FROM employees WHERE id = ?", [
+      req.params.id,
+    ]);
 
-  const [result] = await pool.query("DELETE FROM employees WHERE id = ?", [
-    req.params.id,
-  ]);
+    if (result.affectedRows <= 0)
+      return res.status(404).json({
+        message: "Employee not found",
+      });
 
-  if (result.affectedRows <= 0)
-    return res.status(404).json({
-      message: "Employee not found",
+    // res.send("Employee deleted successfully");
+
+    /* Successful request, but without any content in the response body */
+    res.sendStatus(204);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Something goes wrong",
     });
-
-  // res.send("Employee deleted successfully");
-
-  /* Successful request, but without any content in the response body */
-  res.sendStatus(204);
+  }
 };
--- src/index.js
+++ src/index.js
@@ -13,5 +13,12 @@ app.use(express.json());
 app.use(indexRoutes);
 app.use("/api", employeesRoutes);
 
+/* Middleware function to handle not found, but default is better*/
+// app.use((req, res, next) => {
+//   res.status(404).json({
+//     message: "Endpoint not found",
+//   });
+// });
+
 app.listen(4000);
 console.log("🚀 server running at http://localhost:4000");
